46e88506176c43e08f4cf75cb9c878fa34fb2585
Larissa <larissa@fantasticocomic.com>
2020-09-26 14:53:55 -0500

    Add cors in app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const { config } = require('./config/index');
 const contactsApi = require('./routes/contacts.js');
@@ -7,15 +8,16 @@ const notFoundHandler = require('./utils/middlewares/notFoundHandler.js')
 
 // body parser
 app.use(express.json());
-
+app.options('*', cors())
 contactsApi(app);
 
+
 //middleware for CORS
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "https://oporfinlanding.netlify.app")
-	res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT")
-	next()
-})
+// app.use(function (req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "https://oporfinlanding.netlify.app")
+// 	res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT")
+// 	next()
+// })
 
 //catch 404 err
 app.use(notFoundHandler)
