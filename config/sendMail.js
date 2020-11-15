03d3703828913f351499fd91a9d13394345f8b59
Larissa <larissa@fantasticocomic.com>
2020-11-14 19:32:49 -0600

    Triyn mailtrap

--- config/sendMail.js
+++ config/sendMail.js
@@ -1,14 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transport = nodemailer.createTransport({
-  port: process.env.SMTP_FAN_PORT,
-  host: process.env.SMTP_FAN_HOST,
-  secure: process.env.SMTP_FAN_SECURE,
-  ignoreTLS: process.env.SMTP_FAN_IGNORE_TLS,
+  host: process.env.SMTP_HOST,
+  port: 2525,
   debug: true,
+  logger: true,
   auth: {
-    user: process.env.SMTP_FAN_USER,
-    pass: process.env.SMTP_FAN_PAZZ
+    user: process.env.SMTP_UZER,
+    pass: process.env.SMTP_PAZZ
   },
 });
 
