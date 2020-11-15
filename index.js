0b2a531bb15da1e16aa6267093a2d18a78061cad
Larissa <larissa@fantasticocomic.com>
2020-11-14 20:45:52 -0600

    Change structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const { config } = require('./config/index');
+const { config } = require('./config');
 const contactsApi = require('./routes/contacts.js');
 const { logErrors, errorHandler, wrapErrors } = require('./utils/middlewares/errorHandlers.js')
 const notFoundHandler = require('./utils/middlewares/notFoundHandler.js')
@@ -37,6 +37,6 @@ app.use(errorHandler);
 
 
 app.listen(config.port, function () {
-  console.log(`Listening http://localhost:${config.port}`);
+  console.log(`Listening http://localhost:${config.dockerPort}`);
   
 });
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,20 +3,17 @@ const ContactsService = require('../services/contacts');
 const {
   createContactSchema
 } = require('../utils/schemas/contact')
-
-const transport = require("../config/sendMail");
-
+const transport = require("../utils/sendMail");
 const validationHandler = require('../utils/middlewares/validationHandler')
+const { config } = require('../config');
+const isHuman = require('../utils/middlewares/isHuman')
 
 function contactsApi(app) {
   const router = express.Router();
   app.use('/', router);
-
   const contactsService = new ContactsService();
-
   router.get('/', async function (req, res, next) {
     try {
-
       res.status(200)
       res.send('<h2>OPORFIN API IS RUNNING</h2>')
     } catch (err) {
@@ -30,14 +27,13 @@ function contactsApi(app) {
 
   router.get('/api/contacts', async function (req, res, next) {
     const { tags } = req.query;
-
     try {
       const contacts = await contactsService.getContacts({ tags });
-
       res.status(200).json({
         data: contacts,
         message: 'Contacts Listed'
       });
+
     } catch (err) {
       res.json({
         error: err
@@ -47,17 +43,14 @@ function contactsApi(app) {
   });
 
 
-  router.post('/api/contacts', validationHandler(createContactSchema), async function (req, res, next) {
-    const { body: contact } = req;
+  router.post('/api/contacts', validationHandler(createContactSchema), isHuman, async function (req, res, next) {
+    const { body: contact } = req
     try {
       const createdContact = await contactsService.createContact({ contact });
-      console.log('contact', contact);
-
       await transport.sendMail({
         from: `Contacto de cliente <contacto@oportunidadesfinancieras.com.mx>`,
-        to: process.env.SMTP_EMAIL,
-        cc: process.env.SMTP_BCC,
-        bcc: process.env.SMTP_CC,
+        to: config.SMTP_EMAIL,
+        bcc: config.SMTP_BCC,
         subject: "OPORFIN | Mensaje desde Landingpage",
         html: `<div style="background-color: #f8f8fb; border: 1px solid #e7e7f2; padding: 20px; border-radius: 6px">
         <h1>${contact.contactname}</h1>
@@ -69,15 +62,17 @@ function contactsApi(app) {
         </div>
           `,
       });
-
       res.status(201).json({
-        data: createdContact,
-        message: 'Contact info Sent'
+        data: contact,
+        message: 'Contact info Sent',
+        error: false
       });
     } catch (err) {
       console.log('showed error', err);
-      res.json({
-        error: err
+      res.status(401).json({
+        data: err,
+        message: 'Not created',
+        error: true
       });
       next(err);
     }
